8246acf73f91cd2eefccb6809ebed8c9ddb25973
BricePaquette <brice.paquette@gmail.com>
2022-10-23 10:42:27 -0500

    Finished The map and DrawMap() to display to console

---
+++ EscapeLibrary/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeLibrary
+{
+    public class Character
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Score { get; set; }
+
+        public Character()
+        {
+            X = 5;
+            Y = 1;
+        }
+
+        public void DrawCharacter()
+        {
+
+        }
+    }
+}
