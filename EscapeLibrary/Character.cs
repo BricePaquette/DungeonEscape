5162df9bb870b20bcf4a416e5a5d62169c9f3daf
BricePaquette <brice.paquette@gmail.com>
2022-10-23 11:08:29 -0500

    Completed DrawCharacter()

--- EscapeLibrary/Character.cs
+++ EscapeLibrary/Character.cs
@@ -8,19 +8,35 @@ namespace EscapeLibrary
 {
     public class Character
     {
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();//To allow movement key to be stored
+        char key = 'w';//Key to be pressed
+        char direction = 'u';//To verifiy direction of movement
         public int X { get; set; }
         public int Y { get; set; }
         public int Score { get; set; }
+
+        public List<Position> CharacterPhysicalScore { get; set; }
 
         public Character()
         {
             X = 5;
             Y = 1;
+            Score = 0;
+
+            //Make it like a snake so I can give a score
+            CharacterPhysicalScore = new List<Position>();
+            CharacterPhysicalScore.Add(new Position(X, Y));
         }
 
         public void DrawCharacter()
         {
-
+            foreach (Position position in CharacterPhysicalScore)
+            {
+                Console.SetCursorPosition(position.X, position.Y);//Pull the postion from the list and put the below there
+                Console.Write("■");
+            }
         }
+
+
     }
 }
---
+++ EscapeLibrary/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeLibrary
+{
+    public class Position
+    {
+        //Created so i can add to a list for a score tracker
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        public Position()
+        {
+
+        }
+    }
+}
